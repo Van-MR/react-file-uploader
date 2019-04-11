f113c73a767b33868ac2f2fda41a51dd9a44bac9
vanisher <vannisher@yahoo.com>
2019-04-11 14:52:05 +0800

    set up backend && front-end

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+
+    return (
+      <div className="container">
+          app running
+      </div>
+    );
+
+}
+
+export default App;
